40be73ec8692b1e9a41c9e8d94f73fd6a8109625
Chris Norman <chris.norman1989@outlook.com>
2025-03-12 06:54:48 +0000

    Updated the new game screen.

--- lib/src/screens/new_game.dart
+++ lib/src/screens/new_game.dart
@@ -30,10 +30,10 @@ class NewGame extends StatefulWidget {
 /// State for [NewGame].
 class NewGameState extends State<NewGame> {
   /// The controller for the name of the left player.
-  late final TextEditingController _leftNameController;
+  late final TextEditingController _player1NameController;
 
   /// The controller for the name of the right player.
-  late final TextEditingController _rightNameController;
+  late final TextEditingController _player2NameController;
 
   /// The minimum points for winning the game.
   late int winningPoints;
@@ -54,15 +54,15 @@ class NewGameState extends State<NewGame> {
   @override
   void initState() {
     super.initState();
-    _leftNameController = TextEditingController(text: 'Player 1');
-    _leftNameController.selection = TextSelection(
+    _player1NameController = TextEditingController(text: 'Player 1');
+    _player1NameController.selection = TextSelection(
       baseOffset: 0,
-      extentOffset: _leftNameController.text.length,
+      extentOffset: _player1NameController.text.length,
     );
-    _rightNameController = TextEditingController(text: 'Player 2');
-    _rightNameController.selection = TextSelection(
+    _player2NameController = TextEditingController(text: 'Player 2');
+    _player2NameController.selection = TextSelection(
       baseOffset: 0,
-      extentOffset: _rightNameController.text.length,
+      extentOffset: _player2NameController.text.length,
     );
     winningPoints = 11;
     clearPoints = 2;
@@ -75,8 +75,8 @@ class NewGameState extends State<NewGame> {
   @override
   void dispose() {
     super.dispose();
-    _leftNameController.dispose();
-    _rightNameController.dispose();
+    _player1NameController.dispose();
+    _player2NameController.dispose();
   }
 
   /// Build a widget.
@@ -88,79 +88,85 @@ class NewGameState extends State<NewGame> {
     },
     child: SimpleScaffold(
       title: 'New Game',
-      body: ListView(
-        shrinkWrap: true,
-        children: [
-          TextFormField(
-            autofocus: true,
-            controller: _leftNameController,
-            decoration: const InputDecoration(labelText: 'Left player name'),
-          ),
-          TextFormField(
-            controller: _rightNameController,
-            decoration: const InputDecoration(labelText: 'Right player name'),
-          ),
-          ListTile(
-            title: const Text('Switch Ends'),
-            onTap:
-                () => setState(() {
-                  final temp = _leftNameController.text;
-                  _leftNameController.text = _rightNameController.text;
-                  _rightNameController.text = temp;
-                }),
-          ),
-          PerformableActionsBuilder(
-            actions: [
-              for (final setConfig in [
-                const _SetConfig(
-                  numberOfSets: 1,
-                  key: LogicalKeyboardKey.digit1,
-                ),
-                const _SetConfig(
-                  numberOfSets: 3,
-                  key: LogicalKeyboardKey.digit3,
-                ),
-                const _SetConfig(
-                  numberOfSets: 5,
-                  key: LogicalKeyboardKey.digit5,
-                ),
-              ])
-                PerformableAction(
-                  name: setConfig.numberOfSets.toString(),
-                  invoke:
-                      () =>
-                          setState(() => numberOfSets = setConfig.numberOfSets),
-                  activator: CrossPlatformSingleActivator(setConfig.key),
-                ),
-            ],
-            builder:
-                (final builderContext, final controller) => ListTile(
-                  title: const Text('Number of Sets'),
-                  subtitle: Text(numberOfSets.toString()),
-                  onTap: controller.toggle,
-                ),
-          ),
-          if (numberOfSets > 1)
-            CheckboxListTile(
-              value: switchEnds,
-              onChanged:
-                  (_) => setState(() {
-                    switchEnds = !switchEnds;
-                  }),
-              title: const Text('Switch Ends Between Sets'),
+      body: Form(
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            TextFormField(
+              autofocus: true,
+              controller: _player1NameController,
+              decoration: const InputDecoration(labelText: 'Player 1 name'),
             ),
-          IntListTile(
-            value: numberOfServes,
-            onChanged: (final value) => setState(() => numberOfServes = value),
-            title: 'Number of Serves',
-            min: 1,
-          ),
-        ],
+            TextFormField(
+              controller: _player2NameController,
+              decoration: const InputDecoration(labelText: 'Player 2 name'),
+            ),
+            ListTile(
+              title: const Text('Switch Ends'),
+              onTap:
+                  () => setState(() {
+                    final temp = _player1NameController.text;
+                    _player1NameController.text = _player2NameController.text;
+                    _player2NameController.text = temp;
+                  }),
+            ),
+            PerformableActionsBuilder(
+              actions: [
+                for (final setConfig in [
+                  const _SetConfig(
+                    numberOfSets: 1,
+                    key: LogicalKeyboardKey.digit1,
+                  ),
+                  const _SetConfig(
+                    numberOfSets: 3,
+                    key: LogicalKeyboardKey.digit3,
+                  ),
+                  const _SetConfig(
+                    numberOfSets: 5,
+                    key: LogicalKeyboardKey.digit5,
+                  ),
+                ])
+                  PerformableAction(
+                    name:
+                        // ignore: lines_longer_than_80_chars
+                        '${setConfig.numberOfSets} ${setConfig.numberOfSets == 1 ? "set" : "sets"}',
+                    invoke:
+                        () => setState(
+                          () => numberOfSets = setConfig.numberOfSets,
+                        ),
+                    activator: CrossPlatformSingleActivator(setConfig.key),
+                  ),
+              ],
+              builder:
+                  (final builderContext, final controller) => ListTile(
+                    title: const Text('Number of Sets'),
+                    subtitle: Text(numberOfSets.toString()),
+                    onTap: controller.toggle,
+                  ),
+            ),
+            if (numberOfSets > 1)
+              CheckboxListTile(
+                value: switchEnds,
+                onChanged:
+                    (_) => setState(() {
+                      switchEnds = !switchEnds;
+                    }),
+                title: const Text('Switch Ends Between Sets'),
+              ),
+            IntListTile(
+              value: numberOfServes,
+              onChanged:
+                  (final value) => setState(() => numberOfServes = value),
+              title: 'Number of Serves',
+              min: 1,
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => playGame(context),
         tooltip: 'Start Game',
-        child: const Icon(Icons.play_arrow),
+        child: const Icon(Icons.play_arrow, semanticLabel: 'Start Game'),
       ),
     ),
   );
@@ -168,8 +174,8 @@ class NewGameState extends State<NewGame> {
   /// Start the game with the current settings.
   void playGame(final BuildContext context) => context.pushWidgetBuilder(
     (_) => PlayGame(
-      leftPlayerName: _leftNameController.text,
-      rightPlayerName: _rightNameController.text,
+      leftPlayerName: _player1NameController.text,
+      rightPlayerName: _player2NameController.text,
       winningPoints: winningPoints,
       clearPoints: clearPoints,
       switchEnds: switchEnds,
